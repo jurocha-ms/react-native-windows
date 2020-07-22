42d63a7dd6ec95c32a2cc62900156c328a6776ab
Julio C. Rocha <julio.rocha@microsoft.com>
2020-07-22 00:35:17 -0700

    Initial RN host setup

--- vnext/Microsoft.ReactNative.IntegrationTests/UniversalTestInstance.cpp
+++ vnext/Microsoft.ReactNative.IntegrationTests/UniversalTestInstance.cpp
@@ -6,14 +6,29 @@
 namespace Microsoft::React::Test {
 
 UniversalTestInstance::UniversalTestInstance() noexcept
-    : m_queueController{winrt::Windows::System::DispatcherQueueController::CreateOnDedicatedThread()} {}
+    : m_queueController{winrt::Windows::System::DispatcherQueueController::CreateOnDedicatedThread()} {
+}
 
 #pragma region ITestInstance
 
-void UniversalTestInstance::AttachMeasuredRootView(std::string &&appName) noexcept /*override*/ {}
+void UniversalTestInstance::AttachMeasuredRootView(std::string &&appName) noexcept /*override*/ {
+  m_queueController.DispatcherQueue().TryEnqueue([this]() noexcept {
+    //TODO? Add package providers
 
-void UniversalTestInstance::DetachRootView() noexcept /*override*/
-{}
+    m_host.InstanceSettings().UseDeveloperSupport(true);
+    m_host.InstanceSettings().UseLiveReload(false);
+    m_host.InstanceSettings().UseWebDebugger(false);
+    m_host.InstanceSettings().SourceBundleHost(L"localhost");
+    m_host.InstanceSettings().SourceBundlePort(8081);
+    m_host.InstanceSettings().JavaScriptMainModuleName(L"Dummy");
+    m_host.LoadInstance();
+  });
+}
+
+void UniversalTestInstance::DetachRootView() noexcept /*override*/ {
+  m_host.UnloadInstance().get();
+  m_queueController.ShutdownQueueAsync().get();
+}
 
 std::shared_ptr<facebook::react::Instance> UniversalTestInstance::GetInnerInstance() const noexcept /*override*/ {
   return nullptr;
