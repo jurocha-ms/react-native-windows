b815c98dd770a813f61af51eba662f6f8bfe7c18
Julio C. Rocha <julio.rocha@microsoft.com>
2026-01-16 18:48:15 -0800

    TODO: Obtain main queue

--- vnext/Desktop.IntegrationTests/Prototype.cpp
+++ vnext/Desktop.IntegrationTests/Prototype.cpp
@@ -1,10 +1,44 @@
 //TODO: copyright
 
+#include <CppUnitTest.h>
+
 #include <Microsoft.ReactNative/ReactHost/React.h>
 
-void Foo()
+#include <future>
+
+winrt::Windows::Foundation::IAsyncOperation<bool> Foo()
 {
+  bool result = false;
+  winrt::Microsoft::ReactNative::ReactNativeHost host{};
+  auto settings = host.InstanceSettings();
+  //auto action = host.ReloadInstance();
 
-  winrt::Microsoft::ReactNative::ReactNativeHost m_host{};
+  //action.Completed([&result](auto const &task, winrt::Windows::Foundation::AsyncStatus status)
+  //{
+  //  result = true;
+  //});
+  co_await host.ReloadInstance();
+  result = true;
 
+  co_return result;
 }
+
+TEST_CLASS (Prototype) {
+
+  TEST_METHOD(Proto1)
+  {
+    bool succeeded = true;
+
+    {
+      winrt::Microsoft::ReactNative::ReactNativeHost host{};
+      auto settings = host.InstanceSettings();
+      auto mainQueue = Mso::DispatchQueue::CurrentQueue();
+      auto action = host.ReloadInstance();
+
+      action.get();
+    }
+
+    Microsoft::VisualStudio::CppUnitTestFramework::Assert::IsTrue(succeeded);
+  }
+
+};
