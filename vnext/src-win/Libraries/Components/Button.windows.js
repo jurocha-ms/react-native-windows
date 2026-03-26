e20a3bc97dce3c403677233667dfad68513ad4c5
Vineeth <66076509+vineethkuttan@users.noreply.github.com>
2026-03-26 14:06:21 +0530

    Integrate 0.84.0-nightly-20260107-58bc6c3e3 (#15837)
    * Integrate 0.84.0-nightly-20260107-58bc6c3e3
    
    - https://github.com/facebook/react-native/compare/dd390dbbe...58bc6c3e3
    
    * resolve conflicts
    
    * update react version
    
    * update flow version
    
    * build fix
    
    * update shared filters
    
    * Change files
    
    * update tester files
    
    * update yarn lock

--- vnext/src-win/Libraries/Components/Button.windows.js
+++ vnext/src-win/Libraries/Components/Button.windows.js
@@ -43,7 +43,7 @@ export type ButtonProps = $ReadOnly<{
     Handler to be called when the user taps the button. The first function
     argument is an event in form of [GestureResponderEvent](pressevent).
    */
-  onPress?: (event?: GestureResponderEvent) => mixed,
+  onPress?: (event?: GestureResponderEvent) => unknown,
 
   /**
     If `true`, doesn't play system sound on touch.
@@ -153,7 +153,7 @@ export type ButtonProps = $ReadOnly<{
    */
   accessible?: ?boolean,
   accessibilityActions?: ?$ReadOnlyArray<AccessibilityActionInfo>,
-  onAccessibilityAction?: ?(event: AccessibilityActionEvent) => mixed,
+  onAccessibilityAction?: ?(event: AccessibilityActionEvent) => unknown,
   onAccessibilityTap?: ?() => void, // Windows
   accessibilityState?: ?AccessibilityState,
   accessibilityValue?: ?AccessibilityValue, // Windows
